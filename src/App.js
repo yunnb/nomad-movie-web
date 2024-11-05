fdc043df9bdd1763848e9a7151a34882e5cec5d6
yunnb <chldbsrud010@naver.com>
2024-11-05 21:58:25 +0900

    #7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [toDo, setToDo] = useState('');
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") return;
+        setToDos(prev => [toDo, ...prev]);
+        setToDo("");
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={toDo} type="text" placeholder="Wrtie your to do ..." />
+            <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+            {toDos.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
     </div>
   );
 }
