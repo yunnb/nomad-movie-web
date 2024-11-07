2f20f149ed80b1ab049f2c1c722bbd052dec2a4f
yunnb <chldbsrud010@naver.com>
2024-11-07 16:58:56 +0900

    #7.3 Movie App part One

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [toDo, setToDo] = useState('');
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") return;
-        setToDos(prev => [toDo, ...prev]);
-        setToDo("");
-    };
-  return (
-    <div>
-        <h1>My To Dos ({toDos.length})</h1>
-        <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={toDo} type="text" placeholder="Wrtie your to do ..." />
-            <button>Add To Do</button>
-        </form>
-        <hr />
-        <ul>
-            {toDos.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
-        </ul>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async() => {
+        const json = await (
+            await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies`)
+        ).json();
+        setMovies(json);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+    
+    console.log(movies);
+    return (
+        <div>
+            {loading? <h1>Loading...</h1> :
+                <div>{movies.map(movie =>
+                    <div key={movie.id}>
+                        <h2>{movie.title}</h2>
+                        <p>{movie.overview}</p>
+                    </div>
+                )}</div>
+            }
+
+        </div>
+    );
 }
 
 export default App;
